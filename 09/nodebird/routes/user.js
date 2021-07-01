da2884d47a8e5ea2cddd0560334d9717d4bb97f1
naeunchan <eunchan0492@naver.com>
2021-07-01 15:47:11 +0900

    remove sequelize loggin

--- 09/nodebird/routes/user.js
+++ 09/nodebird/routes/user.js
@@ -8,33 +8,32 @@ const router = express.Router();
 router.post("/:id/follow", isLoggedIn, addFollowing);
 router.post("/:id/unfollow", isLoggedIn, removeFollowing);
 
-// router.post("/:id/profile", isLoggedIn, async (req, res, next) => {
-//   try {
-//     console.log(req);
-//     const user = await User.findOne({
-//       where: {
-//         id: req.user.id,
-//       },
-//     });
+router.post("/:id/profile", isLoggedIn, async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        id: req.user.id,
+      },
+    });
 
-//     if (user) {
-//       if (password === "") {
-//         await user.update({
-//           nick,
-//         });
-//       } else {
-//         await user.update({
-//           nick,
-//           password,
-//         });
-//       }
-//       res.send("success");
-//     } else {
-//       res.status(404).send("no user");
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     next(error);
-//   }
-// });
+    if (user) {
+      if (password === "") {
+        await user.update({
+          nick,
+        });
+      } else {
+        await user.update({
+          nick,
+          password,
+        });
+      }
+      res.send("success");
+    } else {
+      res.status(404).send("no user");
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 module.exports = router;
