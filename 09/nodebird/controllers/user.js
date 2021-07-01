c52e73c57e2a2e6270f24041252d11a35105eea6
naeunchan <eunchan0492@naver.com>
2021-07-01 16:31:26 +0900

    update to user's profile function is finished

--- 09/nodebird/controllers/user.js
+++ 09/nodebird/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 exports.addFollowing = async (req, res, next) => {
   try {
@@ -28,6 +29,39 @@ exports.removeFollowing = async (req, res, next) => {
     });
     if (user) {
       await user.removeFollowing(parseInt(req.params.id, 10));
+      res.send("success");
+    } else {
+      res.status(404).send("no user");
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+exports.updateProfile = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        id: req.user.id,
+      },
+    });
+
+    const { nick, password } = req.body;
+
+    if (user) {
+      if (password === "") {
+        await user.update({
+          nick,
+        });
+      } else {
+        const hash = await bcrypt.hash(password, 12);
+
+        await user.update({
+          nick,
+          password: hash,
+        });
+      }
       res.send("success");
     } else {
       res.status(404).send("no user");
--- 09/nodebird/routes/user.js
+++ 09/nodebird/routes/user.js
@@ -1,39 +1,12 @@
 const express = require("express");
 
 const { isLoggedIn } = require("./middlewares");
-const { addFollowing, removeFollowing } = require("../controllers/user");
+const { addFollowing, removeFollowing, updateProfile } = require("../controllers/user");
 
 const router = express.Router();
 
 router.post("/:id/follow", isLoggedIn, addFollowing);
 router.post("/:id/unfollow", isLoggedIn, removeFollowing);
+router.post("/:id/profile", isLoggedIn, updateProfile);
 
-router.post("/:id/profile", isLoggedIn, async (req, res, next) => {
-  try {
-    const user = await User.findOne({
-      where: {
-        id: req.user.id,
-      },
-    });
-
-    if (user) {
-      if (password === "") {
-        await user.update({
-          nick,
-        });
-      } else {
-        await user.update({
-          nick,
-          password,
-        });
-      }
-      res.send("success");
-    } else {
-      res.status(404).send("no user");
-    }
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-});
 module.exports = router;
